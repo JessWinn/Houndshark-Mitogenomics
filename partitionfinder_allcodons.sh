7b9b199d61da3c94efe69f89342b58aae5e82252
Jessica Winn <112689721+JessWinn@users.noreply.github.com>
2022-09-05 14:54:37 +0200

    Maximum Likelihood Analysis

---
+++ partitionfinder_allcodons.sh
@@ -0,0 +1,23 @@
+### PARTITIONING & MODEL SELECTION ###
+
+#!/bin/bash
+#PBS -l nodes=1:ppn=8
+#PBS -N partitions_allcodons
+#PBS -M 21634076@sun.ac.za  
+#PBS -m abe
+#PBS -e partitions_allcodons.err
+#PBS -o partitions_allcodons.out
+#PBS -l walltime=01:00:00
+
+#Declare working directory from which to submit PBS script
+cd $PBS_O_WORKDIR
+
+# Load IQTree 1.6.12
+module load app/IQTREE/1.6.12
+
+## 1. Export the final concatenated cleaned alignment as a phylip file from Geneious.
+
+## 2. Create a NEXUS file containing annotation information
+
+iqtree -s /home/21634076/Carcharhiniform_mitogenomics/ML/Galeomorphi_cleaned_alignment.phy -spp /home/21634076/Carcharhiniform_mitogenomics/ML/partitions_allcodons/partitions_allcodons.nex -m MF+MERGE -rcluster 30 -AICc -safe -nt AUTO
+
